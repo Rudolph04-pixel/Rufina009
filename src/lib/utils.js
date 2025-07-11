bd5dc4b7c0076315aab45716de4d2f40cdff980c
Rudolph04-pixel <rudolph.shg@gmail.com>
2025-07-10 21:08:37 -0400

    feat: add utils helper required by shadcn

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,10 +2,7 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Combina clases condicionalmente y aplica twMerge
- *  - Uso: cn("p-4", isActive && "bg-blue-500")
- */
+/** Combina clases condicionalmente */
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
